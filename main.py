944b7523d58730aa1d6043ee98fca414ed822d89
bryce080 <142543275+bryce080@users.noreply.github.com>
2025-03-21 16:33:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-x = input("Type a number from 1-100")
+x = int(input("Type a number from 1-100:"))
 
 random_num = random.randint(1, 100)
 
 while x != random_num:
-    if int > x true:
-        input("Higher.")
-    elif int < x true:
-        input("Lower.")
+    if random_num > x:
+        x = int(input("Higher."))
+    else:
+        random_num < x
+        x = int(input("Lower."))
+else:
+    if random_num == x:
+        print("Correct!")
